ec14e9c2c9dc6076770e292efe2f4558455a0234
Adnan Abbas <madnanabbas14@gmail.com>
2020-05-03 11:39:29 +0500

    Updates

---
+++ precache-manifest.414dcefd7adbae6c0d3c0782ab061057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c3a6a93c1d631d80308322826bd96e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9148cef21e7a7f6d5e76",
+    "url": "/monsters-rolodex/static/css/main.60605a87.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9148cef21e7a7f6d5e76",
+    "url": "/monsters-rolodex/static/js/main.f5847a38.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
